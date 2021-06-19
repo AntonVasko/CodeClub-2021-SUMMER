72e40bbfbd7c46612c45dbc1fa20ac1edbba1eae
AntonVasko <vasko.anton.a@gmail.com>
2021-06-20 01:03:53 +0300

    Create Challenge2.py

---
+++ 01. 23.06.2021/Challenge2.py
@@ -0,0 +1,17 @@
+#Challenge: about yourself
+print('My favourite animals are sheep')
+print('''
+Here's a picture of a dog:
+o-###-
+  | |   #
+''')
+
+print('I live in Glasgow')
+print(''' 
+    _|_
+   |   |
+   |#  |____
+   |   |    |
+   |  #|  # |
+  _|___|_#__|_
+''')
