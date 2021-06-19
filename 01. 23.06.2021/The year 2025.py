ba2680319ad61ef0c86100ec225c3878a64ff555
AntonVasko <vasko.anton.a@gmail.com>
2021-06-20 01:11:59 +0300

    Create The year 2025.py

---
+++ 01. 23.06.2021/The year 2025.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+#The year 2025
+print('My favourite animals are sheep')
+print('''
+Here's a picture of a dog:
+o-###-
+  | |   #
+''')
+
+print('I live in Glasgow')
+print(''' 
+    _|_
+   |   |
+   |#  |____
+   |   |    |
+   |  #|  # |
+  _|___|_#__|_
+''')
+
+born = int(input("What year were you born? "))
+age = 2025 - born
+print('In the year 2025 you\'ll be', age, 'years old')
+
