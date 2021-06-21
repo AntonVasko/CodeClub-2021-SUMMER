5fc836df728fe67bebc567260d26ed002fcaef6c
AntonVasko <vasko.anton.a@gmail.com>
2021-06-22 00:01:06 +0300

    Create Turtle Race - Challenge More lines.py

---
+++ 01. 23.06.2021/Turtle Race - Challenge More lines.py
@@ -0,0 +1,15 @@
+#Challenge: More lines
+from turtle import *
+speed(10)
+penup()
+goto(-140, 140)
+for step in range(15):
+  write(step, align = 'center')
+  right(90)
+  forward(10)
+  pendown()
+  forward(150)
+  penup()
+  backward(160)
+  left(90)
+  forward(20)
