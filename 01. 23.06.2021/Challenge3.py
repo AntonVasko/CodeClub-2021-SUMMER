4d0f7de3110fc2de75ffe723a0bb4471174ac624
AntonVasko <vasko.anton.a@gmail.com>
2021-06-21 13:04:53 +0300

    Create Challenge3.py

---
+++ 01. 23.06.2021/Challenge3.py
@@ -0,0 +1,8 @@
+#Challenge: your age in dog years
+age = int(input("What is your age? "))
+age_dog = age * 7
+print('If your were a dog, you\'d be ', age_dog, ' !!')
+print('''
+o____ 
+ ||||   
+''')
