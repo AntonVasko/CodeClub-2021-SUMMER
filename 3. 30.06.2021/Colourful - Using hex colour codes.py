1d4f417ea90009160117bb9924a67d6ab08cf22a
AntonVasko <vasko.anton.a@gmail.com>
2021-06-27 21:13:12 +0300

    Create Colourful - Using hex colour codes.py

---
+++ 3. 30.06.2021/Colourful - Using hex colour codes.py
@@ -0,0 +1,12 @@
+#Colourful Creations. Using hex colour codes. Hello
+
+from turtle import *
+
+screen = Screen()
+screen.setup(400, 400)
+screen.bgcolor ('#EAEF89')
+
+color('#89D6EF')
+style = ('Arial', 40, 'bold')
+write('Hello', font=style, align='center')
+hideturtle()
