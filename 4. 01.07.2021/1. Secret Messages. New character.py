929384006190d3daf4376955ec50d5a4a02f3fa0
AntonVasko <vasko.anton.a@gmail.com>
2021-07-03 18:34:48 +0300

    Create 1. Secret Messages. New character.py

---
+++ 4. 01.07.2021/1. Secret Messages. New character.py
@@ -0,0 +1,11 @@
+#Secret Messages. New character
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 3
+character = input('Please enter a character ')
+position = alphabet.find(character)
+print('Position of a character ', character, ' is ', position)
+newPosition = (position + key) % 26
+print('New position of a character ', character, ' is ', newPosition)
+newCharacter = alphabet[newPosition]
+print('New character of a new position of ', newPosition, ' is ', newCharacter)
