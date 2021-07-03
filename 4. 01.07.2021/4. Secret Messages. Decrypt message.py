16c0f70c4c56590952137bf6838eccf9349cda94
AntonVasko <vasko.anton.a@gmail.com>
2021-07-03 19:05:12 +0300

    Create 4. Secret Messages. Decrypt message.py

---
+++ 4. 01.07.2021/4. Secret Messages. Decrypt message.py
@@ -0,0 +1,21 @@
+#Secret Messages. Decrypt message
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = int(input('Please input key '))
+decMessage = ''
+
+message = (input('Please enter a message: '))
+
+for character in message:
+  if character in alphabet:
+    position = alphabet.find(character)
+    #print('Position of a character ', character, ' is ', position)
+    newPosition = (position - key) % 26
+    #print('New position of a character ', character, ' is ', newPosition)
+    newCharacter = alphabet[newPosition]
+    #print('New character of a new position of ', newPosition, ' is ', newCharacter)
+    decMessage += newCharacter
+  else:
+    decMessage += character
+  
+print('New encrypt message of your ', message, ' is ', decMessage)
